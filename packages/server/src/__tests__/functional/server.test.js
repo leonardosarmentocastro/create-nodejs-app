ec909449000b2a9cfd7b36080b455434eb80f36a
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2021-05-03 15:04:27 -0300

    tests - server is customizable (routes can be added)

--- packages/server/src/__tests__/functional/server.test.js
+++ packages/server/src/__tests__/functional/server.test.js
@@ -5,8 +5,24 @@ const { server } = require('../../server');
 
 test('server can start and is reachable', async t => {
   const port = 8080;
-  await server.start(port);
+  const api = await server.start(port);
 
   const body = await got(`http://127.0.0.1:${port}/`).json();
   t.deepEqual(body, { application: 'up' });
+
+  await server.close(api);
+});
+
+test('server is customizable (routes can be added)', async t => {
+  const port = 8080;
+  const api = await server.start(port, {
+    connect: (app) => {
+      app.get('/custom-route', (req, res) => res.json({ customizable: true }))
+    },
+  });
+
+  const body = await got(`http://127.0.0.1:${port}/custom-route`).json();
+  t.deepEqual(body, { customizable: true });
+
+  await server.close(api);
 });
