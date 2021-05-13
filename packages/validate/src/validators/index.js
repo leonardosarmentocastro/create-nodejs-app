01942ec67a2788c24bf0f22b4d807087d762b65e
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2021-05-12 22:59:47 -0300

    export

--- packages/validate/src/validators/index.js
+++ packages/validate/src/validators/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('./is-already-in-use-validator'),
+  ...require('./is-password-strong-validator'),
   ...require('./is-required-validator'),
   ...require('./is-too-long-validator'),
   ...require('./is-valid-email-validator'),
