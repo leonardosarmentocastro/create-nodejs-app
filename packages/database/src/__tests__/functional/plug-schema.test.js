b269a6a0bcb3522e3bf6586bb35325d6461006ff
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2021-05-13 18:45:03 -0300

    plug-schema: now inherits 'virtuals'

--- packages/database/src/__tests__/functional/plug-schema.test.js
+++ packages/database/src/__tests__/functional/plug-schema.test.js
@@ -9,11 +9,12 @@ test.before('setup database and test model', async t => {
   await database.connect();
 
   const schema = new mongoose.Schema({ text: String });
+  schema.set('toObject', { virtuals: true });
   schema.plugin(plugSchema(commonSchema));
   t.context.model = new mongoose.model('Model', schema);
 });
 
-test('must inherit "transform" operations from plugged schema (e.g. commonsSchema)', async t => {
+test('must inherit "toObject.transform" operations from plugged schema (e.g. commonsSchema)', async t => {
   const doc = await t.context.model.create({ text: 'any' });
   const transformedDoc = doc.toObject();
 
@@ -27,4 +28,11 @@ test('must inherit "fields" from plugged schema (e.g. commonsSchema)', async t =
 
   t.truthy(transformedDoc.createdAt);
   t.truthy(transformedDoc.updatedAt);
+});
+
+test('must inherit "toObject.virtuals" from plugged schema (e.g. commonsSchema)', async t => {
+  const doc = await t.context.model.create({ text: 'any text' });
+  const transformedDoc = doc.toObject();
+
+  t.truthy(transformedDoc.id);
 });
--- packages/database/src/plug-schema.js
+++ packages/database/src/plug-schema.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-
 const plugTransformations = (fromSchema, toSchema) => {
   const newTransformation = fromSchema.options.toObject?.transform;
   if (!newTransformation) return; // can't plug if there is nothing to plug!
 
   const ownTransformation = toSchema.options.toObject?.transform;
-  if (!ownTransformation) return toSchema.set('toObject', { transform: newTransformation });
+  if (!ownTransformation) return toSchema.set('toObject', {
+    ...toSchema.options.toObject,
+    transform: newTransformation,
+  });
 
   // apply own transformation before new transformations
   toSchema.set('toObject', {
+    ...toSchema.options.toObject,
     transform: (doc, ret, options) => {
       const transformed = ownTransformation(doc, ret, options);
       return newTransformation(doc, transformed, options);
